f067ecac6e6c343e8124e8873d6e4f6d638879b2
Soypete <miriah.peterson@getweave.com>
2019-04-02 18:41:47 -0600

    add server

--- client/glient.go
+++ client/glient.go
@@ -1,6 +1,12 @@
 package client
 
-import "context"
+import (
+	"context"
+
+	pb "github.com/soypete/example_grpc_k8s/proto"
+)
+
+type server struct{}
 
 func (s *server) SearchHouses(ctx context.Context, in *pb.Parameters) (*pb.Results, error) {
 	return &pb.Results{}, nil
