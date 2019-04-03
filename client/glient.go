771a66f163e2247aeaf06bb8804b4d2f18d98a39
Soypete <miriah.peterson@getweave.com>
2019-04-02 19:36:50 -0600

    skeletion

--- client/glient.go
+++ client/glient.go
@@ -2,12 +2,33 @@ package client
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
 
-	pb "github.com/soypete/example_grpc_k8s/proto"
+	pb "github.com/soypete/example_grpc_k8s/gengo"
+	"google.golang.org/grpc"
 )
 
 type server struct{}
 
-func (s *server) SearchHouses(ctx context.Context, in *pb.Parameters) (*pb.Results, error) {
+func (s *server) FindHouse(ctx context.Context, in *pb.Parameters) (*pb.Results, error) {
 	return &pb.Results{}, nil
+}
+
+func main() {
+	serverAddr := flag.String("addr", "8082", "port to connect to grpc server.")
+	flag.Parse()
+	conn, err := grpc.Dial(*serverAddr)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.RealEstateClient(conn)
+
+	feature, err := client.FindHouse(context.Background(), &pb.Parameters{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(feature)
 }
--- server/server.go
+++ server/server.go
@@ -1,3 +1,31 @@
 package server
 
-type server struct{}
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/soypete/example_grpc_k8s/gengo"
+	"google.golang.org/grpc"
+)
+
+type RealEstateServer struct{}
+
+func (s *RealEstateServer) FindHouse(ctx context.Context, in *pb.Parameters) (*pb.Results, error) {
+	return &pb.Results{}, nil
+}
+
+func main() {
+	port := flag.String("port", "8082", "port that grpc server is exposed on")
+
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterRealEstateServer(grpcServer, &RealEstateServer{})
+	grpcServer.Serve(lis)
+}
